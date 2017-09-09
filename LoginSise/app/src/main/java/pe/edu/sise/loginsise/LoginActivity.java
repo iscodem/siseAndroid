7916451a4a38cbd977cc916db4e5736d85069084
David Paul Porras Cordova <iscodem@gmail.com>
2017-09-08 21:58:44 -0500

    Nueva modificacion

--- LoginSise/app/src/main/java/pe/edu/sise/loginsise/LoginActivity.java
+++ LoginSise/app/src/main/java/pe/edu/sise/loginsise/LoginActivity.java
@@ -1,13 +1,25 @@
 package pe.edu.sise.loginsise;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editUsuario, editPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editUsuario= (EditText) findViewById(R.id.editUsuario);
+        editPassword= (EditText) findViewById(R.id.editPassword);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
     }
 }
