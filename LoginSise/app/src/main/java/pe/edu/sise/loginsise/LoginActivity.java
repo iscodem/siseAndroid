df6d14b422589d27dcbf07b6d22b17a78effc7d4
David Paul Porras Cordova <iscodem@gmail.com>
2017-09-08 22:04:58 -0500

    Nueva modificacion

--- LoginSise/app/src/main/java/pe/edu/sise/loginsise/LoginActivity.java
+++ LoginSise/app/src/main/java/pe/edu/sise/loginsise/LoginActivity.java
@@ -1,10 +1,13 @@
 package pe.edu.sise.loginsise;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +23,21 @@ public class LoginActivity extends AppCompatActivity {
         editPassword= (EditText) findViewById(R.id.editPassword);
 
         btnLogin = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+    public void ingresar(View v){
+
+        String usuario = editUsuario.getText().toString();
+        String password = editPassword.getText().toString();
+
+        if(usuario.equals("")||password.equals("")){
+            Toast.makeText(this, "Los datos no pueden ser vacios.", Toast.LENGTH_SHORT).show();
+        }else{
+            Intent intent = new Intent(this,MainActivity.class);
+
+            
+        }
 
     }
 }
