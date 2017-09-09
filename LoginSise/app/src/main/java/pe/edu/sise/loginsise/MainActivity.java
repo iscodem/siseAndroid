29d97d4c68a8e31fc2029defa18b6ee384b7ace8
David Paul Porras Cordova <iscodem@gmail.com>
2017-09-08 22:30:11 -0500

    Nueva modificacion

--- LoginSise/app/src/main/java/pe/edu/sise/loginsise/MainActivity.java
+++ LoginSise/app/src/main/java/pe/edu/sise/loginsise/MainActivity.java
@@ -1,7 +1,9 @@
 package pe.edu.sise.loginsise;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+
+        String usuario = (String) intent.getExtras().get("usuario");
+        String password = (String) intent.getExtras().get("password");
+
+        Toast.makeText(this, "usuario:"+usuario, Toast.LENGTH_SHORT).show();
 
     }
 }
